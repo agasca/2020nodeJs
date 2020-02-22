4a328ffd7fbd488c2f1c2fc60e3ab97a23aef42e
agasca <profesorweb@yahoo.com>
2020-02-21 21:40:11 -0600

    inicio Middleware

--- web/express/appExpress.js
+++ web/express/appExpress.js
@@ -1,62 +1,86 @@
 c = console.log
-
-//localhost:3000
-
 const express = require(`express`)
-const app = express()
+const app = express() //localhost:3000
 
-//ejemplo 3 middleware
-let isLogin = () => true
-let logger = (req, res, next) => {
-    c(`Peticion de tipo`, req.method)
-    next()
-}
-let showIP = (req, res, next) => {
-    c(`IP: 127.0.0.1`)
-    next()
-}
-app.use((req, res, next) => {
-    if(isLogin()){
-        next()
-    }else{
-        res.send(`no estas logueados`)
-    }
-}, logger, showIP)
-
-//app.use(logger)
+c("inicia")
 
 /*
 //ejemplo 1
 app.get('/', function (req, res) {
-    res.send('Hello World!');
-  });
-  
-app.listen(3000, function () {
-    console.log('Example app listening on port 3000!');
+  res.send('Hello World!');
 });
+
+//ejemplo 2  Fn=>
+app.get('/', (req, res) => {
+   res.send(`Hello World! ${req.method}`);
+  });
 */
 
-//ejemplo 2
-//app.get('/', (req, res) => {
-//    res.send(`Hello World! ${req.method}`);
- // });
-app.get('/:user', (req, res) => {
-    let usuario = req.params.user
-    res.send(`Bienvenido ${usuario}`);
-  });
-  
+
+//
+//Middleware
+let isLogin = () => true //cambiar a false y comprobae en POSTMAN
+//ejemplo 3.2
+let logger = (req, res, next) => {
+  c(`Peticion de tipo`, req.method)
+  next()
+}
+
+//ejemplo 3.3
+let showIP = (req, res, next) => {
+  c(`IP: 127.0.0.1`)
+  next()
+}
+
+//ejemplo 3.1 Middleware (hasta antes del response)
+//res.send(`Bienvenido ${usuario}`);
+app.use((req, res, next) => {
+  if(isLogin()){
+    next()
+  }else{
+      res.send(`no estas logueado`)
+  }
+}, logger, showIP) //ejemplo 3.2 & ejeplo 3.3 incluida en la funcion, revisar en el chrome y en la terminal
+//   |       |    
+//   |       -------Middleware x+1    
+//   ---------------Middleware x       
+
+//ejemplo 3.2
+//app.use(logger)   //revisar en el chrome y en la terminal
+
+//ejemplo 3
+//metodos identificados por http GET, POST, PUT, DELETE
+//se utiliza la aplicacion POSTMAN para comprobar
+app.get('/', (req, res) => {
+  //res.send(`hola`)
+  res.send(`Metodo ${req.method}`);
+});
+
 app.post('/', (req, res) => {
-    res.send(`Hello World! ${req.method}`);
-  });
+  res.send(`Metodo ${req.method}`);
+});
 
 app.put('/', (req, res) => {
-    res.send(`Hello World! ${req.method}`);
-  });
+  res.send(`Metodo ${req.method}`);
+});
 
 app.delete('/', (req, res) => {
-    res.send(`Hello World! ${req.method}`);
-  });
-
-app.listen(3000, () => {
-    console.log('Example app listening on port 3000!');
+  res.send(`Metodo ${req.method}`);
 });
+
+
+
+
+/*
+//
+//funcion de servidor listener
+app.listen(3000, function () {
+  c('Example app listening on port 3000!');
+});
+*/
+//funcion flecha
+app.listen(3000, () => {
+  c('Example app listening on port 3000!');
+})
+
+c("termina")
