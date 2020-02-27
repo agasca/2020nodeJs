e3a0f5637bfa8daa9c8d71d9c35a84d691204ab1
agasca <profesorweb@yahoo.com>
2020-02-26 19:51:50 -0600

    token

---
+++ proyecto_node-crudToken/node-crudToken/routes/personaForm.js
@@ -0,0 +1,54 @@
+let express = require('express');
+let router = express.Router();
+
+let mongoose = require('./../config/conexion');
+let Persona = require('./../models/persona');
+
+const c = console.log
+
+router.post('/persona/operar', (req, res, next) => {
+  //c(req.body);  
+
+  if (req.body._id === "") {
+    //llamar a la rutina para crear:
+    let per = new Persona({
+      nombres: req.body.nombres,
+      apellidos: req.body.apellidos,
+      token: token(nomApe(req.body.nombres), nomApe(req.body.apellidos))
+      //edad: req.body.token
+    });    
+    per.save();
+  } else {    
+    //console.log(req.body._id);
+    Persona.findByIdAndUpdate(req.body._id, { $set: req.body }, { new: true }, (err, model) => {
+      if (err) throw err;
+    });
+  }
+  res.redirect('/');
+});
+
+
+let nomApe = (cadena) => {
+  this.data = cadena
+  this.min = 0
+  this.max =  this.data.length
+  this.posData =  Math.random() * (this.max - this.min) + this.min 
+
+  return this.data.substr(this.posData,2)
+}
+
+let token = (n, a) => {
+  this.nombre = n
+  this.apellido = a
+  this.fechaToken = new Date(Date.now())   
+  this.r = Math.random().toString(36).substring(7) //36 es la base, 2 es binario, 16 es hexa
+  this.resultado = ""
+  this.resultado= (this.nombre + 
+                  this.apellido + "@" +
+                  this.fechaToken.toString().slice(16,24) + 
+                  this.r)
+
+  return this.resultado
+}
+
+module.exports = router;
